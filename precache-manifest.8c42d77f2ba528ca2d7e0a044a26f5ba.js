89b7a8efd89753f88d2ce9202297096986a9ea2e
Eric Sheng <ericsheng21@gmail.com>
2020-05-12 11:43:18 -0700

    Updates

---
+++ precache-manifest.8c42d77f2ba528ca2d7e0a044a26f5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3e51d37d8023a629e98bd90440324a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "84bdb13f91a4db4dae56",
+    "url": "/react-portfolio/static/css/main.a52e94c5.chunk.css"
+  },
+  {
+    "revision": "bd67d11c09f1662c612d",
+    "url": "/react-portfolio/static/js/2.b7a52389.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.b7a52389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84bdb13f91a4db4dae56",
+    "url": "/react-portfolio/static/js/main.9d73580b.chunk.js"
+  },
+  {
+    "revision": "03ab6e8d60ab9149a550",
+    "url": "/react-portfolio/static/js/runtime-main.14be16bc.js"
+  }
+]);
